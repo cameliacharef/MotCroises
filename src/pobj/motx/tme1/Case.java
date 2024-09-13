0b30ad61c8346305a066d65de37500b26aa80d3c
21105280 <21105280@ppti-14-308-05.ufr-info-p6.jussieu.fr>
2024-09-13 16:45:55 +0200

    case succès

--- src/pobj/motx/tme1/Case.java
+++ src/pobj/motx/tme1/Case.java
@@ -1,5 +1,32 @@
 package pobj.motx.tme1;
-
+	
 public class Case {
-
+	private int ligne;
+	private int colonne;
+	private char valeur;
+	
+	public Case(int lig, int col, char c) {
+		ligne=lig;
+		colonne=col;
+		valeur=c;
+	}
+	public int getLig(){
+		return ligne;
+	}
+	public int getCol(){
+		return colonne;
+	}
+	public char getChar(){
+		return valeur;
+	}
+	public void setChar(char c){
+		valeur = c;
+	}
+	public boolean isVide() {
+		return valeur == ' ';
+	}
+	public boolean isPleine() {
+		return valeur == '*';
+	}
+	
 }
