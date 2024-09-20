357573b62c7d907d0d9d88d16f8eb0d75d4fc606
marya <marya@DESKTOP-A1BJNTL.mshome.net>
2024-09-20 22:22:55 +0200

    GrillePlaces ver1

--- src/pobj/motx/tme2/GrillePlaces.java
+++ src/pobj/motx/tme2/GrillePlaces.java
@@ -10,26 +10,59 @@ public class GrillePlaces {
 	
 	public GrillePlaces (Grille grille) {
 		this.grille = grille;
+		for(int i = 0; i < grille.nbLig(); i++) {
+			List<Case> lig = getLig(i);
+			cherchePlaces(lig);
+		}
+		for(int j = 0; j < grille.nbCol(); j++) {
+			List<Case> col = getCol(j);
+			cherchePlaces(col);
+		}
 	}	
 	
 	public List<Emplacement> getPlaces(){
-		
+		return places;
 	}
+	
 	public int getNbHorizontal() {
-		
+		int res = 0;
+		for(Emplacement e : places)
+			if(e.getCase(0).getLig() == e.getCase(1).getLig())
+				res++;
+		return res;
 	}
 	
 	@Override public String toString() {
-		
+		String s = "";
+		for(Emplacement e : places)
+			s += e + "\n";
+		return s;
 	}
 	
+	
 	private List<Case> getLig(int lig){
-		
+		List<Case> res = new ArrayList<Case>();
+		for(int j = 0; j < grille.nbCol(); j++)
+			res.add(grille.getCase(lig, j));
+		return res;
 	}
+	
 	private List<Case> getCol(int col){
-		
+		List<Case> res = new ArrayList<Case>();
+		for(int i = 0; i < grille.nbLig(); i++)
+			res.add(grille.getCase(i, col));
+		return res;
 	}
 	private void cherchePlaces(List<Case> cases) {
-		
+		Emplacement e = new Emplacement();
+		for(Case c : cases) {
+			if(!c.isPleine())
+				e.add(c);
+			else{
+				if(e.size() > 2)	
+					places.add(e);
+				e = new Emplacement();
+			}
+		}
 	}
 }
